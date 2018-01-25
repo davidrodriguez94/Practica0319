00046eda808317fc1c2f297191ccc9483c3fc33b
davidrodriguez94 <rodriguezdiezdavid94@gmail.com>
2018-01-25 12:07:59 +0100

    Ejercicio nº 3 implementado

---
+++ Instrumento.java
@@ -0,0 +1,84 @@
+/**
+ * Write a description of class Instrumento here.
+ *
+ * @author (David)
+ * @version (24-01-2018)
+ */
+public class Instrumento
+{
+    // Tipo de instrumento: cuerda, viento, percusion...
+    private String tipoInstrumento;
+    // Marca del instrumento
+    private String marcaInstrumento;
+    // Precio del instrumento
+    private int precioInstrumento;
+    // Define un numero identificativo
+    private int id;
+    /**
+     * Constructor de la clase Instrumento
+     */
+    public Instrumento(String tipoInstrumento, String marcaInstrumento, int precioInstrumento)
+    {
+        this.tipoInstrumento = tipoInstrumento;
+        this.tipoInstrumento = marcaInstrumento;
+        precioInstrumento = 0;
+    }
+
+    /**
+     * Método que define el tipo de instrumento 
+     */
+    public void setTipoInstrumento()
+    {
+        this.tipoInstrumento = tipoInstrumento;
+    }
+
+    /**
+     * Método que retorna el tipo de instrumento
+     */
+    public String getTipoInstrumento()
+    {
+        return tipoInstrumento;
+    }
+
+    /**
+     * Método que define la marca de instrumento 
+     */
+    public void setMarcaInstrumento()
+    {
+        this.tipoInstrumento = marcaInstrumento;
+    }
+
+    /**
+     * Método que retorna la marca de instrumento 
+     */
+    public String getMarcaInstrumento()
+    {
+        return marcaInstrumento;
+    }
+
+    /**
+     * Método que define el precio de instrumento 
+     */
+    public void setPrecioInstrumento()
+    {
+        precioInstrumento = 0;
+    }
+
+    /**
+     * Método que retorna el precio de instrumento 
+     */
+    public int getPrecioInstrumento()
+    {
+        return precioInstrumento;
+    }
+    
+    /**
+     * Método que devuelva un String con
+     * todas la características del objeto en formato texto
+     */
+    public String devolverCaracteristicas()
+    {
+        String detallesProducto = "Tipo: " + tipoInstrumento + "Marca: " +  marcaInstrumento + "Precio: " + precioInstrumento + "ID: " + id; 
+        return detallesProducto;
+    }
+}
---
+++ Tienda.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Tienda here.
+ *
+ * @author (David Rodriguez)
+ * @date (24-01-2018  10:47)
+ */
+public class Tienda
+{
+    // Array que guarda los instrumentos
+    private ArrayList <Instrumento> instrumentos;
+    // Define un numero identificativo
+    private int id;
+    /**
+     * Constructor de Tienda
+     */
+    public Tienda()
+    {
+        instrumentos = new ArrayList<Instrumento>();
+        id = 0;
+    }
+    
+    /**
+     * Añade instrumentos
+     */
+    public void addInstrumento(String instrumento, String marca, int precio)
+    {
+        Instrumento nuevoInstrumento = new Instrumento(instrumento,marca,precio);
+        instrumentos.add(nuevoInstrumento);
+    }
+}
